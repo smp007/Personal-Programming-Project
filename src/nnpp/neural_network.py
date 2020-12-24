eb638e85762070c1a35c4bfb2b8c9bf6f4957b21
Sudarsan <sudarsanmpai.1@gmail.com>
2020-12-25 00:55:37 +0100

    Implemented stochastic gradient descent

--- src/nnpp/neural_network.py
+++ src/nnpp/neural_network.py
@@ -23,8 +23,6 @@ for file in file_list:
                 n=int(no_of_atoms[0])
                 number_of_atomss.append(n)
 
-#print(energy_list)
-
 
 tic = time.time()
 def test_train_split(filelist,energylist,split):
@@ -52,39 +50,20 @@ toc = time.time()
 test_xy = ([(np.loadtxt(os.path.join('./symmetry_functions','%s') %(x[:-3]+'txt')),y)for x,y in zip(b,d)]) #np.loadtxt(os.path.join('./symmetry_functions','%s') %(x[:-3]+'txt'))
 train_xy = ([(np.loadtxt(os.path.join('./symmetry_functions','%s') %(x[:-3]+'txt')),y)for x,y in zip(a,c)])
 
-#print(a[0],b[0])
-#print((test_xy),train_xy[0][0])
-
-
-# z = np.linspace(-100,100,10000)
-# print(leaky_ReLU(z))
-
-# #plt.plot(z,leaky_ReLU(z))
-# plt.plot(z,sigmoid(z))
-# plt.plot(z,tanh(z))
-# #plt.plot(z,ReLU(z))
-# plt.grid('True')
-# plt.show()
-
-x6 = np.array([1,2,3,4])
-x7 = np.array([6,8,3,1])
-x8 = [[1,2,3,4],[6,8,3,1],[2,3,4,5],[5,4,3,2],[6,7,5,4],[0,3,4,3]]
-y8 = np.array([92])
-#print(x1,x2)
-
 #-------------------------------------------------------------------------------
 #Activation functions
 #-------------------------------------------------------------------------------
 
 def sigmoid(z):
-    s = 1/(1+np.exp(-1*z))
+    s = 1.0/(1+np.exp(-z))
     return s 
 
 def ReLU(z):
     return np.maximum(0,z)
 
 def tanh(z):
-    val = (np.exp(z)-np.exp(-z))/(np.exp(z)+np.exp(-z))
+    #val = (np.exp(z)-np.exp(-z))/(np.exp(z)+np.exp(-z))
+    val = np.tanh(z)
     return val
 
 def linear(z):
@@ -99,7 +78,7 @@ def d_sigmoid(s):
 
 def d_ReLU(s):
     #derivative of ReLU
-    return np.where(s>=0,1,0)
+    return 1.0 *(s>0)
 
 def d_tanh(s):
     #derivative of tanh
@@ -107,6 +86,13 @@ def d_tanh(s):
 
 def d_linear(s):
     return 1
+
+ActivationFunction = {
+    'sigmoid' : (sigmoid,d_sigmoid),
+    'ReLU'    : (ReLU,d_ReLU),
+    'tanh'    : (tanh,d_tanh),
+    'linear'  : (linear,d_linear),
+    }
 
 E_nn = np.array([1,2,3])
 E_ref = np.array([1,1,1])
@@ -127,13 +113,6 @@ def MSE(predictions,references):
 def d_MSE(predictions,references):
     return(predictions-references)
 
-    
-
-print('rmse',RMSE(E_nn,E_ref))
-print('mae',MAE(E_nn,E_ref))
-print('mse',MSE(E_nn,E_ref))
-
-
 
 #-------------------------------------------------------------------------------
 
@@ -144,44 +123,43 @@ class NeuralNetwork:
         self.hidden_layer1_nodes = nodelist[1]        
         self.hidden_layer2_nodes = nodelist[2]        
         self.output_nodes = nodelist[-1]
-        self.layer1_activation = activations[0]
-        self.layer2_activation = activations[1]
-        self.output_activation = activations[-1]
+        self.layer1_activation = ActivationFunction[activations[0]][0]
+        self.layer2_activation = ActivationFunction[activations[1]][0]
+        self.output_activation = ActivationFunction[activations[-1]][0]
+        self.layer1_der_activation = ActivationFunction[activations[0]][1]
+        self.layer2_der_activation = ActivationFunction[activations[1]][1]
+        self.output_der_activation = ActivationFunction[activations[-1]][1]
 
         self.weights1 = np.random.randn(self.input_nodes,self.hidden_layer1_nodes)
         #print(self.weights1)
-        self.bias1 = 0
+        self.bias1 = np.zeros((1,self.hidden_layer1_nodes),dtype=float)
         self.weights2 = np.random.randn(self.hidden_layer1_nodes,self.hidden_layer2_nodes)
         #print(self.weights2)
-        self.bias2 = 0
+        self.bias2 = np.zeros((1,self.hidden_layer2_nodes),dtype=float)
         self.weights3 = np.random.randn(self.hidden_layer2_nodes,self.output_nodes)
-        self.bias3 = 0
+        self.bias3 = np.zeros((1,self.output_nodes),dtype=float)
 
     def __repr__(self):
         return f"This is a {self.input_nodes}-{self.hidden_layer1_nodes}-{self.hidden_layer2_nodes}-{self.output_nodes} neural network"
 
         
     def forward_prop(self,x):
-        self.layer1 = self.layer1_activation(np.dot(x,self.weights1)+self.bias1)
-        self.layer2 = self.layer2_activation(np.dot(self.layer1,self.weights2)+self.bias2)
-        output      = self.output_activation(np.dot(self.layer2,self.weights3)+self.bias3)
+        self.layer1 = self.layer1_activation(np.dot(x,self.weights1))#+self.bias1)
+        self.layer2 = self.layer2_activation(np.dot(self.layer1,self.weights2))#+self.bias2)
+        output      = self.output_activation(np.dot(self.layer2,self.weights3))#+self.bias3)
         return output
 
     
     def backward_prop(self,x,e_nn,e_ref):
         #output layer
         self.error_output_layer = d_MSE(e_nn,e_ref)
-        ##print('error_output_layer',self.error_output_layer)
-        self.delta_output_layer = self.error_output_layer * d_linear(e_nn)
-        ##print('delta_output_layer',self.delta_output_layer)
+        self.delta_output_layer = self.error_output_layer * self.output_der_activation(e_nn)
         #layer 2
         self.error_layer2 = self.delta_output_layer.dot(self.weights3.T)
-        ##print('error_layer2',self.error_layer2)
-        self.delta_layer2 = self.error_layer2 * d_sigmoid(self.layer2)
-        ##print('delta_layer2',self.delta_layer2)
+        self.delta_layer2 = self.error_layer2 * self.layer2_der_activation(self.layer2)
         #layer 1
         self.error_layer1 = self.delta_layer2.dot(self.weights2.T)
-        self.delta_layer1 = self.error_layer1 * d_sigmoid(self.layer1)
+        self.delta_layer1 = self.error_layer1 * self.layer1_der_activation(self.layer1)
 
         #weight update term(derivatives)
         dJdw1 =  x.T.dot(self.delta_layer1)
@@ -253,6 +231,19 @@ def numerical_gradients(nn,x,e_nn,e_ref):
 
 #------------------------------------------------------------------------------- dec 19 night
 def nn_switcher(x):
+    """
+    Returns the index which is used the switch the atomic NNs
+    ie. if there are 6 atoms out of which 2 are Ti and 4 are O,we need to switch
+    from Ti_NN to O_NN after 2 elements in the 6 element array.
+
+    Arguments : 
+    x -- 2d array of symmetry functions[G] with shape [n x 70]
+
+    Returns : 
+    no_of_ti_atoms[val] -- no of Ti atoms in the structure,so as to switch NN 
+    after that value
+
+    """
     val = len(x)
     no_of_ti_atoms = {
       # No of atoms : No of Ti atoms
@@ -268,6 +259,22 @@ def nn_switcher(x):
     return no_of_ti_atoms[val]
 
 def structure_forward_prop(a,nn1,nn2,index):
+    """
+    Predicts the total structural energy with the help of atomic NNs
+    
+    Arguments : 
+    a -- 3d array of symmetry functions[G] with shape [n x 1 x 70]
+    nn1 -- object of class Neuralnetwork
+    nn2 -- another object of class Neuralnetwork
+    index -- return value of nn_switcher function
+
+    Returns : 
+    output -- array of predicted individual atomic energies
+    ie if there are 6 atoms out of which 2 are Ti and 4 are O,first 2 [G]s of Ti 
+    are passed through Ti_NN and remaining 4 [G]s of O are passed through O_NN 
+    and all the 6 values are stored in an array 
+
+    """
     output=[]
     for i in range(len(a)):
         if i<= (index-1):        
@@ -276,52 +283,58 @@ def structure_forward_prop(a,nn1,nn2,index):
             output.append(nn2.forward_prop(a[i]))
     return (output)
 
-def train(nn1,nn2,index,a,e_ref,learning_rate):
+def weight_array_init(nn1):
+    r_,d_ = np.zeros(nn1.weights1.shape),np.zeros(nn1.weights1.shape)
+    s_,e_ = np.zeros(nn1.weights2.shape),np.zeros(nn1.weights2.shape)
+    t_,f_ = np.zeros(nn1.weights3.shape),np.zeros(nn1.weights3.shape)
+    return r_,d_,s_,e_,t_,f_ 
+
+
+
+def structure_backward_prop(a,nn1,nn2,index,output,e_ref,learning_rate):#try only for bkwd prop sum
+    r,d,s,e,t,f = weight_array_init(nn1)
+    for i in range(len(a)):
+        if i<= (index-1):
+            r += nn1.backward_prop(a[i],output,e_ref)[0]
+            #print('r',r)
+            s += nn1.backward_prop(a[i],output,e_ref)[1]
+            #print('s',s)
+            t += nn1.backward_prop(a[i],output,e_ref)[2]
+        else:
+            d += nn2.backward_prop(a[i],output,e_ref)[0]
+            e += nn2.backward_prop(a[i],output,e_ref)[1]
+            #print('e',e)
+            f += nn2.backward_prop(a[i],output,e_ref)[2]
+    nn1.NN_optimize(r,s,t,learning_rate)
+    nn2.NN_optimize(d,e,f,learning_rate)
+
+def train(nn1,nn2,index,a,e_ref,learning_rate):#working
     output = sum(structure_forward_prop(a,nn1,nn2,index))  
     print('output',output,'e_ref',e_ref) 
     w1_t,w2_t,w3_t = nn1.backward_prop(a[0],output,e_ref)
-    #print('w1',w1_t,'w2',w2_t,'w3',w3_t)
+    print('w1',w1_t,'w2',w2_t,'w3',w3_t)
     nn1.NN_optimize(w1_t,w2_t,w3_t,learning_rate)
     w1_o,w2_o,w3_o = nn2.backward_prop(a[index],output,e_ref)
-    #print('w1',w1_o,'w2',w2_o,'w3',w3_o)
+    print('w1',w1_o,'w2',w2_o,'w3',w3_o)
     nn2.NN_optimize(w1_o,w2_o,w3_o,learning_rate)
 
 
-##node_list1 = [4,3,3,1]          #contains the layer sizes
-#activations = [sigmoid,sigmoid,sigmoid]    
-#nn_Ti = NeuralNetwork(node_list1,activations)
-#nn_O  = NeuralNetwork(node_list,activations) 
+def train2(nn1,nn2,index,a,e_ref,learning_rate):#try only
+    output = sum(structure_forward_prop(a,nn1,nn2,index))
+    structure_backward_prop(a,nn1,nn2,index,output,e_ref,learning_rate)
 
-#print(nn_Ti)
 
-# nn_Ti.weights1 = np.ones(nn_Ti.weights1.shape)
-# nn_O.weights1 = np.ones(nn_O.weights1.shape)
-# nn_Ti.weights2 = np.ones(nn_Ti.weights2.shape)
-# nn_O.weights2 = np.ones(nn_O.weights2.shape)
-
-#print(nn_Ti.layer1,'\n',nn_Ti.layer2)
-#print(nn_O.weights1,'\n',nn_O.weights2)
-
-# no_of_atoms = len(test_xy[0][0])
-# print(no_of_atoms)
-
-'''#70-10-10-1 nn -----------------------------------------------------------------
+#70-10-10-1 nn -----------------------------------------------------------------
 file_name = 'structure1249.txt'
 x = np.loadtxt(os.path.join('./symmetry_functions','%s') %file_name)
 n = len(x)
 A = x.reshape(n,1,70)
 print(A)
-E_ref = [[0]]  #-19960.66173260
-#print(len(E_ref))
-#print(len(x))
-#print(x[0])
-
-#o1 = nn_Ti.forward_prop(x1)
-#o2 = nn_O.forward_prop(x2)
+E_ref = [[-4987.12739129]]  #-19960.66173260
 print(A[0].shape)
 #print(o1)
 node_list = [70,10,10,1]          #contains the layer sizes
-activations = [sigmoid,sigmoid,linear]    
+activations = ['sigmoid','sigmoid','linear']    
 nn_Ti = NeuralNetwork(node_list,activations)
 nn_O  = NeuralNetwork(node_list,activations)
 print('Ti --',nn_Ti,'\n','O --',nn_O)
@@ -331,120 +344,75 @@ print('Ti --',nn_Ti,'\n','O --',nn_O)
 
 #print('Weights 1 \n',nn_Ti.weights1,'\n','Weights 2 \n',nn_Ti.weights2,'\n','Weights 3 \n',nn_Ti.weights3)
 
-def nn_switcher(x):
-    val = len(x)
-    no_of_ti_atoms = {
-      # No of atoms : No of Ti atoms
-                 6  :   2,                
-                22  :   8,
-                23  :   8,
-                24  :   8,
-                46  :   16,
-                47  :   16,
-                94  :   32,
-                95  :   32
-    }
-    return no_of_ti_atoms[val]
-
 index = nn_switcher(A)
 
 #print('no of ti :',index)
 
 
-def structure_forward_prop(a,nn1,nn2):
-    output=[]
-    for i in range(len(a)):
-        if i<= (index-1):        
-            output.append(nn1.forward_prop(a[i]))
-        else:
-            output.append(nn2.forward_prop(a[i]))
-    return (output)
 
-print(structure_forward_prop(A,nn_Ti,nn_O))
-        
-def train(nn1,nn2,index,a,e_ref):
-    output = sum(structure_forward_prop(a,nn1,nn2))  
-    print('output',output,'e_ref',e_ref) 
-    w1_t,w2_t,w3_t = nn1.backward_prop(a[0],output,e_ref)
-    #print('w1',w1_t,'w2',w2_t,'w3',w3_t)
-    nn1.NN_optimize(w1_t,w2_t,w3_t,learning_rate)
-    w1_o,w2_o,w3_o = nn2.backward_prop(a[0],output,e_ref)
-    #print('w1',w1_o,'w2',w2_o,'w3',w3_o)
-    nn2.NN_optimize(w1_o,w2_o,w3_o,learning_rate)
 
-for i in range(130):
-    train(nn_Ti,nn_O,index,A,E_ref)
 
-print(structure_forward_prop(A,nn_Ti,nn_O))
-print('output',sum(structure_forward_prop(A,nn_Ti,nn_O)),'e_ref',E_ref)
-print('cost',sum(structure_forward_prop(A,nn_Ti,nn_O))-E_ref)
+
+for i in range(1250):
+    #train(nn_Ti,nn_O,index,A,E_ref)
+    train2(nn_Ti,nn_O,index,A,E_ref,learning_rate=4e-5)
+
+print(structure_forward_prop(A,nn_Ti,nn_O,index))
+print('output',sum(structure_forward_prop(A,nn_Ti,nn_O,index)),'e_ref',E_ref)
+print('cost',sum(structure_forward_prop(A,nn_Ti,nn_O,index))-E_ref)
 # for i in range(2):
 #     train(a,E_ref)
 # # print((output))
+
 # # print(sum(output))
 #70-10-10-1 nn -----------------------------------------------------------------'''
 
-'''##Test for a small network------------------------------------------------------
-node_list = [2,4,3,1]
-activations = [sigmoid,sigmoid,linear]  
-x = np.array([6.4,2.9]).reshape(1,2)
-y = np.array([-88.9051]).reshape(1,1)
-print(x.shape)
-print(y.shape)
-nn_test = NeuralNetwork(node_list,activations)
 
-def train(a,b):
-    out = nn_test.forward_prop(a)
-    w1,w2,w3 = nn_test.backward_prop(a,out,b)
-    print(w1,'\n',w2,'\n',w3)
-    nn_test.NN_optimize(w1,w2,w3,learning_rate)
 
-print('before',nn_test.forward_prop(x))
-for i in range(5):
-    train(x,y)
-
-print('after',nn_test.forward_prop(x))
-print('cost',nn_test.forward_prop(x)-y)
-#------------------------------------------------------------------------------'''
-#print(structure_forward_prop(a))
 
 
 print('Time taken =',str((toc-tic)) + 'sec')
 
-'''#numerical gradient check starts------------------------------------------------
+def stochastic_gradient_descent(a_list,e_ref_list,learning_rate,epochs):
 
-node_list = [2,3,5,1]
-activations = [sigmoid,sigmoid,linear]  
-x = np.array([2,20]).reshape(1,2)
-y = np.array([1000]).reshape(1,1)
-print(x.shape)
-print(y.shape)
-nn_test = NeuralNetwork(node_list,activations)
+    m = len(e_ref_list)
+    cost_extract = np.zeros(epochs)
+    for i in range(epochs):
+        cost = 0
+        e_nn_list = []
+        for j in range(m):
+            train2(nn_Ti,nn_O,index,a_list[j],e_ref_list[j],learning_rate)
+            e_nn_list.append(sum(structure_forward_prop(a_list[j],nn_Ti,nn_O,index)))
+        print('predicted',e_nn_list,'\n','reference',e_ref_list) 
+        cost = MSE(np.asarray(e_nn_list),np.asarray(e_ref_list))        
+        cost_extract[i] = cost
 
-print('Weights 1 \n',nn_test.weights1,'\n','Weights 2 \n',nn_test.weights2,'\n','Weights 3 \n',nn_test.weights3)
-
-def train(a,b):
-    out = nn_test.forward_prop(a)
-    w1,w2,w3 = nn_test.backward_prop(a,out,b)
-    print(w1,'\n',w2,'\n',w3)
-    #print(nn_test.backward_prop(a,out,b))
-    ################nn_test.NN_optimize(w1,w2,w3,learning_rate)
-    #print(nn_test.backward_prop(a,out,b))
-
-print('before',nn_test.forward_prop(x))
-for i in range(1):
-    train(x,y)
-
-print('after',nn_test.forward_prop(x))
-print('error',nn_test.forward_prop(x)-y)
+    return cost_extract
 
 
-print('Weights 1 \n',nn_test.weights1,'\n','Weights 2 \n',nn_test.weights2,'\n','Weights 3 \n',nn_test.weights3)
-print(nn_test.set_parameters(nn_test.collect_parameters()))
-e_nn = nn_test.forward_prop(x)
-print(nn_test.analytical_gradients(x,e_nn,y))
-print(numerical_gradients(nn_test,x,e_nn,y))
-print(nn_test.analytical_gradients(x,e_nn,y)-numerical_gradients(nn_test,x,e_nn,y))
-#print(np.linalg.norm(nn_test.analytical_gradients(x,e_nn,y)-numerical_gradients(nn_test,x,e_nn,y)))
+'''
 
-#numerical gradient check ends--------------------------------------------------'''
+file_name_list = ['structure0005.txt','structure0004.txt','structure0003.txt','structure0002.txt','structure0001.txt']#,'structure1249.txt']
+X_list = [np.loadtxt(os.path.join('./symmetry_functions','%s') %file_name) for file_name in file_name_list]
+A_list = [x.reshape(len(x),1,70) for x in X_list]
+print(A_list[1].shape)
+E_ref_list = [[-19960.74194513],[-19960.78597929],[-19960.75811714],[-19960.69526834],[-19960.66173260]]#,[-4987.12739129]]
+#print(A_list[2].shape)
+#print(len(E_ref_list))
+
+#E_ref_list = np.asarray([[-4987.12739129],[-78964.89340133],[-19960.66173260]])
+#E_nn_list = np.asarray([[-4987],[-78964],[-19960]])
+
+#print(MSE(E_nn_list,E_ref_list))
+#print(stochastic_gradient_descent(A_list,E_ref_list,learning_rate=4e-5,epochs=150))
+
+cost_variation = stochastic_gradient_descent(A_list,E_ref_list,learning_rate=4e-5,epochs=40)
+
+fig = plt.figure(figsize = (6,4),dpi =150)
+plt.plot(cost_variation,'o')
+plt.xlabel('epochs')
+plt.ylabel('cost')
+plt.title('Cost variation for a very small dataset')
+plt.show()
+fig.tight_layout()
+fig.savefig('cost_variation_5_dataset.png')'''
